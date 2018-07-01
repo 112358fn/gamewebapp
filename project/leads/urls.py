b3deaa164cad8df20c100396e6e30e4ef278b2d2
Alonso, Alvaro Rodrigo <112358.fn@gmail.com>
2018-07-01 14:35:26 +0200

    Added rest api endpoint to update the team info

--- project/leads/urls.py
+++ project/leads/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('api/team/', views.TeamListAPI.as_view() ),
     path('api/team/<int:pk>/', views.TeamRetriveAPI.as_view() ),
+    path('api/team/update/<int:pk>/', views.TeamRetriveUpdateAPI.as_view() ),
     path('api/distance/', views.DistanceCreateAPI.as_view()) ,
 ]
 
