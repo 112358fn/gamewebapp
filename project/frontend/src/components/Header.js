b7c319aa218aeec32e3916e8c60ee966adb37803
Kondrakov, Timur <shadowmurdoc777@gmail.com>
2018-07-05 13:06:55 +0200

    Header layout & logo, added custom css

--- project/frontend/src/components/Header.js
+++ project/frontend/src/components/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-    <div className="container-fluid" style={{verticalAlign:'baseline'}}>
-        <div className="row">
-            <div className="col-xs-8">
-                <h4>Brand</h4>
+    <div className="container" style={{verticalAlign:'baseline'}}>
+        <div className="row app-header">
+            <div className="col-xs-5 col-sm-4 col-md-3">
+                <img src="./images/partinator_logo.png" className="img-responsive app-header-logo" alt="Partinator" />
             </div>
-            <div className="col-xs-2">
-                <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-            </div>
-            <div className="col-xs-2">
-                <NavLink to="/help" activeClassName="is-active" exact={true}>Help</NavLink>
+            <div className="col-xs-7 col-sm-8 col-md-9">
+                <ul className="list-inline text-right app-header-menu">
+                    <li>
+                        <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/help" activeClassName="is-active" exact={true}>Help</NavLink>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
